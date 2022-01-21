d60eb0249c650def22b9404c9a30e1fe7b08cc35
Munieshwar Ramdass <mramdass@live.com>
2022-01-21 10:32:40 -0500

    Update merge_bytes.py

--- merge_bytes.py
+++ merge_bytes.py
@@ -12,15 +12,20 @@ def read_bytes(path):
     with open(path, 'rb') as reader:
         return reader.read()
 
-def merge_bytes(path, extension):
-    base, name, ext = name_parts(path)
+def merge_bytes(path):
+    path_base, path_name, path_ext = name_parts(path)
     for root, dirs, files in os.walk(path):
-        with open(f'{name}.{extension}', 'wb') as writer:
+        try:
+            base, name, ext = name_parts(os.path.join(root, files[0]))
+        except Exception as e:
+            print(f'merge_bytes.py: {str(e)}')
+            exit(1)
+        with open(f'{path_name}{ext}', 'wb') as writer:
             for f in files:
                 part = read_bytes(os.path.join(root, f))
                 writer.write(part)
     return
 
 if __name__=='__main__':
-    merge_bytes(sys.argv[1], sys.argv[2])
+    merge_bytes(sys.argv[1])
     print('fin')
